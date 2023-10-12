51d3c4fdd7231915b50d2e7107e7d952122f8f85
AlsoFabio <110996759+AlsoFabio@users.noreply.github.com>
2023-10-12 09:26:31 -0300

    arreglo de metodos

--- src/app.service.ts
+++ src/app.service.ts
@@ -17,25 +17,46 @@ export class AppService {
     }
   ]
   getListProducts(): Array<Object> {
-    return this.products;
+    try {
+      return this.products;
+    } catch (error) {
+      throw new Error("Error en getListProduct" + JSON.stringify(error));
+    }
   }
 
   getOneProduct(id): any {
-    return this.products.find(product => product.id === parseInt(id));
+    try {
+      return this.products.find(product => product.id === parseInt(id));
+    } catch (error) {
+      throw new Error("Error en getOneProduct" + JSON.stringify(error));
+    }
   }
 
   postProduct(datos): any {
-    this.products.push(datos)
-    return datos
+    try {
+      this.products.push(datos)
+      return datos
+    } catch (error) {
+      throw new Error("Error en postProduct" + JSON.stringify(error));
+    }
   }
 
   putProduct(id, datos): any {
     const indice = this.products.findIndex(product => product.id === parseInt(id));
-    return Object.assign(this.products[indice], datos)
+    try {
+      this.products[indice] = { ...this.products[indice], ...datos };
+      return this.products[indice];
+    } catch (error) {
+      throw new Error("Error en putProduct" + JSON.stringify(error));
+    }
   }
 
   deleteProduct(id): any {
-    this.products.splice(this.products.findIndex(obj => obj.id === parseInt(id)), 1)
-    return this.products
+    try {
+      this.products.splice(this.products.findIndex(obj => obj.id === parseInt(id)), 1)
+      return this.products
+    } catch (error) {
+      throw new Error("Error en deleteProduct" + JSON.stringify(error));
+    }
   }
 }
