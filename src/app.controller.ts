ae920401b7224e1fcfa8ac4bbf0124a4a42b31d5
AlsoFabio <110996759+AlsoFabio@users.noreply.github.com>
2023-10-26 09:55:16 -0300

    limpieza de los modulos principales

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,33 +1,44 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller()
+@Controller('app')
 export class AppController {
 
-
-  constructor(private readonly appService: AppService) { }
-
-  @Get()
-  getHello(): Array<Object> {
-    return this.appService.getListProducts();
-  }
-  @Get(':id')
-  getProduct(@Param("id") id: string): any {
-    return this.appService.getOneProduct(id);
+  @Get('')
+  getHello(@Param('name') name: string): string {
+    return 'hola! ' + name;
   }
 
-  @Post()
-  sendData(@Body() datos): any {
-    return this.appService.postProduct(datos);
-  }
-
-  @Put(':id')
-  updateData(@Body() datos,@Param('id') id): any {
-    return this.appService.putProduct(id,datos);
-  }
-
-  @Delete(':id')
-  killData(@Param("id") id: string) {
-    return this.appService.deleteProduct(id)
-  }
 }
+// import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+
+
+//   constructor(private readonly appService: AppService) { }
+
+//   @Get()
+//   getHello(): Array<Object> {
+//     return this.appService.getListProducts();
+//   }
+//   @Get(':id')
+//   getProduct(@Param("id",new ParseUUIDPipe({version:"4"})) id: string): any {
+//     return this.appService.getOneProduct(id);
+//   }
+
+//   @Post()
+//   sendData(@Body() datos: Object): any {
+//     return this.appService.postProduct(datos);
+//   } 
+
+//   @Put(':id')
+//   updateData(@Param('id',new ParseUUIDPipe({version:"4"})) id: string, @Body() datos: Object): any {
+//     return this.appService.putProduct(id, datos);
+//   }
+
+//   @Delete(':id')
+//   killData(@Param("id",new ParseUUIDPipe({version:"4"})) id: string) {
+//     return this.appService.deleteProduct(id)
+//   }
+// }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,65 +1,74 @@
-import { Injectable, Param } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  private products = [
-    {
-      id: uuidv4().toString(),
-      name: "product 1",
-      price: 1000,
-      stock: 25,
-    },
-    {
-      id: uuidv4().toString(),
-      name: "product 2",
-      price: 500,
-      stock: 50,
-    }
-  ]
-  getListProducts(): Array<Object> {
-    try {
-      return this.products;
-    } catch (error) {
-      throw new Error("Error en getListProduct" + JSON.stringify(error));
-    }
-  }
-
-  getOneProduct(id): any {
-    try {
-      return this.products.find(product => product.id === parseInt(id));
-    } catch (error) {
-      throw new Error("Error en getOneProduct" + JSON.stringify(error));
-    }
-  }
-
-  postProduct(datos): any {
-    try {
-      const datoFormateado={...datos,id:uuidv4().toString()}
-      this.products.push(datoFormateado)
-      return datoFormateado
-    } catch (error) {
-      throw new Error("Error en postProduct" + JSON.stringify(error));
-    }
-  }
-
-  putProduct(id, datos): any {
-    const indice = this.products.findIndex(product => product.id === parseInt(id));
-    try {
-      this.products[indice] = { ...this.products[indice], ...datos };
-      return this.products[indice];
-    } catch (error) {
-      throw new Error("Error en putProduct" + JSON.stringify(error));
-    }
-  }
-
-  deleteProduct(id): any {
-    const indice = this.products.findIndex(product => product.id === parseInt(id));
-    try {
-      this.products.splice(indice, 1)
-      return this.products
-    } catch (error) {
-      throw new Error("Error en deleteProduct" + JSON.stringify(error));
-    }
+  getHello(): string {
+    return 'Hello World!';
   }
 }
+// import { Injectable } from '@nestjs/common';
+// import { v4 as uuidv4 } from 'uuid';
+
+// @Injectable()
+// export class AppService {
+//   private products = [
+//     {
+//       id: uuidv4().toString(),
+//       name: "product 1",
+//       price: 1000,
+//       stock: 25,
+//     },
+//     {
+//       id: uuidv4().toString(),
+//       name: "product 2",
+//       price: 500,
+//       stock: 50,
+//     }
+//   ]
+//   getListProducts(): Array<Object> {
+//     try {
+//       return this.products;
+//     } catch (error) {
+//       throw new Error("Error en getListProduct" + JSON.stringify(error));
+//     }
+//   }
+
+//   getOneProduct(id:string): any {
+//     try {
+//       console.log(this.products.find(product => product.id === id));
+//       return this.products.find(product => product.id === id);
+//     } catch (error) {
+//       throw new Error("Error en getOneProduct" + JSON.stringify(error));
+//     }
+//   }
+
+//   postProduct(datos:any): any {
+//     try {
+//       const datoFormateado={id:uuidv4().toString(),...datos}
+//       this.products.push(datoFormateado)
+//       return datoFormateado
+//     } catch (error) {
+//       throw new Error("Error en postProduct" + JSON.stringify(error));
+//     }
+//   }
+
+//   putProduct(id:string, datos:Object): any {
+//     const indice = this.products.findIndex(product => product.id === id);
+//     try {
+//       this.products[indice] = { ...this.products[indice], ...datos };
+//       return this.products[indice];
+//     } catch (error) {
+//       throw new Error("Error en putProduct" + JSON.stringify(error));
+//     }
+//   }
+
+//   deleteProduct(id:string): any {
+//     const indice = this.products.findIndex(product => product.id === id);
+//     try {
+//       this.products.splice(indice, 1)
+//       return this.products
+//     } catch (error) {
+//       throw new Error("Error en deleteProduct" + JSON.stringify(error));
+//     }
+//   }
+// }
